7f91b148ece2f20a61246932bca48e8c72c71a1d
EsperSaN <polawat.ruenruay@hotmail.com>
2023-08-16 09:02:43 +0000

    adjust the 1st push

--- minihell.c
+++ minihell.c
@@ -1,48 +1,23 @@
 #include "minishell.h"
-typedef struct s_environment
-{
-	char *key;
-	char *value;
-	struct s_environment *next;
-}	t_env;
-typedef struct s_amd
-{
-	int redirect; // <----- redirect flag bool 0 is default if have redirect chage to 1
-	int pipo[2];	// <--- for pipe init to [0,0]
-	int fd_in;	// <------ default is 0 set by open after if have file_in;
-	char *file_in; // <--- the file name that user pass in command (command < eiei.txt) want "eiei.txt"
-	char **command;	// <-- the split command {"ls" , "-la", "......"}
-	char *file_out; // <-- file out str for output (command > eiei.txt) want eiei.txt
-	int fd_out;	// default 1 <set by open if have file_out
-	struct	s_amd *next;
-}	t_cmd ;
-
-typedef struct s_token
-{
-	struct t_env 	*env_token; // <------ store env argument that pass by main (char **env) to use as minishell env
-	char			**env; // <------- env to pass in to execve function must be char ** (join after token)
-	struct t_cmd	*command;
-
-}	t_tok; // <-- can change later kub
 
 int main(int ac , char **av, char **env)
 {
 	(void) av;
 
-	if (ac != 1 || !env)
+	if (ac != 1 || !env) // << รับเเค่ รันโปรเเกรมโดยไม่ใส่ av ครับ "./minishell เท่านั้น"
 		return (1);
-	//t_tok token;
+	//t_tok token; 
 	while (1)
 	{
 		char *input;
-		input = readline(PROMPT);
-		if (input == NULL)
+		input = readline(PROMPT); //<< PROMPT in the header
+		if (input == NULL) // << check case EOF or ^C kub 
 		{
-			rl_clear_history();
-			exit(1);
+			rl_clear_history(); //<< safety 1st
+			exit(1); // << have to figure out the exit code later
 		}
 		printf("input is [%s]\n", input);
-		add_history(input);
-		free(input);
+		add_history(input); // << กด ขึ้นเพื่อดู command ก่แนหน้าได้
+		free(input); 
 	}
 }
--- minishell.h
+++ minishell.h
@@ -5,5 +5,32 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+typedef struct s_environment
+{
+	char *key;
+	char *value;
+	struct s_environment *next;
+}	t_env;
+typedef struct s_amd
+{
+	int redirect; // <----- redirect flag bool 0 is default if have redirect chage to 1
+	int pipo[2];	// <--- for pipe init to [0,0]
+	int fd_in;	// <------ default is 0 set by open after if have file_in;
+	char *file_in; // <--- the file name that user pass in command (command < eiei.txt) want "eiei.txt"
+	char **command;	// <-- the split command {"ls" , "-la", "......"}
+	char *file_out; // <-- file out str for output (command > eiei.txt) want eiei.txt
+	int fd_out;	// default 1 <set by open if have file_out
+	struct	s_amd *next;
+}	t_cmd ;
+
+typedef struct s_token
+{
+	struct t_env 	*env_token; // <------ store env argument that pass by main (char **env) to use as minishell env
+	char			**env; // <------- env to pass in to execve function must be char ** (join after token)
+	struct t_cmd	*command;
+
+}	t_tok; // <-- can change later kub
+
+
 #define PROMPT "MINISHELL > "
 #endif
