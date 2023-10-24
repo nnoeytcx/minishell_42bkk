6b42efa7d7730bb25827ffeb53a69a99e50d5d14
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-10-24 13:24:10 +0000

    tailwind :cleanup

--- env_handle/env_util.c
+++ env_handle/env_util.c
@@ -1,55 +1,12 @@
 #include "../minishell.h"
 
-size_t	ft_strlen(const char *s)
+void	print_env(t_tok token)
 {
-	size_t	i;
-
-	i = 0;
-	while (*s)
+	t_env *tmp = token.env_token;
+	while (tmp->next)
 	{
-		i++;
-		s++;
+		printf("[%s]=[%s]\n", tmp->key, tmp->value);
+		tmp = tmp->next;
 	}
-	return (i);
-}
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	while (n)
-	{
-		if (*s1 != *s2)
-			return (((unsigned char)*s1) - ((unsigned char)*s2));
-		if (*s1 == 0)
-			return (0);
-		n--;
-		s1++;
-		s2++;
-	}
-	return (0);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	while (n)
-	{
-		*(unsigned char *)s = 0;
-		s++;
-		n--;
-	}
-	return ;
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*malzero;
-	size_t	len;
-
-	if (count == SIZE_MAX || size == SIZE_MAX)
-		return (NULL);
-	len = size * count;
-	malzero = malloc(len);
-	if (malzero == NULL)
-		return (NULL);
-	ft_bzero(malzero, len);
-	return (malzero);
+	printf("[output = %s]\n",get_value_from_key(tmp->key, token.env_token));
 }
