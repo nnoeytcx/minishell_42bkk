3e84ded87efe5f4fa7b16b05b45002db80c02af8
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-11-23 06:30:23 +0000

    TEST : add parser->exe test and add g_variable signal

--- minihell.c
+++ minihell.c
@@ -1,9 +1,13 @@
 #include "minishell.h"
 
+
+int g_signal = 0 ;
+
 int main(int ac , char **av, char **env)
 {
 	(void) av;
 
+	//the env is char * with no \n
 	if (ac != 1 || !env) // << รับเเค่ รันโปรเเกรมโดยไม่ใส่ av ครับ "./minishell เท่านั้น"
 		return (1);
 	t_tok token;
@@ -19,15 +23,18 @@ int main(int ac , char **av, char **env)
 			rl_clear_history(); //<< safety 1st
 			exit(1); // << have to figure out the exit code later
 		}
+		printf("input is [%s]\n", input);
 	//	printf("input is [%s]\n", input);
 		mock_up(&token, input);
 		printf("out mock\n");
 		print_tok(token);
+		exe_command(&token);
 		// printf("----------out mock-----------\n");
 		// print_tok(token);
 		if (!ft_strncmp(input, "env", ft_strlen("env")))
 			print_env(token);
-		add_history(input); // << กด ขึ้นเพื่อดู command ก่แนหน้าได้
+		if (!(0 == ft_strlen(input)))
+			add_history(input); // << กด ขึ้นเพื่อดู command ก่แนหน้าได้
 		// make_token(token, input); // <<-- lexer parser
 		// run_token(token); //<< executor
 		// free_input_and_cmd_token(input, &token.command);
