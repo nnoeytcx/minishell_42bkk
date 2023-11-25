b3d7578d6515050868e23e910e9b7e1246f40c9f
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-11-25 09:21:32 +0000

    Parser : add free function [NOT DONE]

---
+++ parser/free_token_data.c
@@ -0,0 +1,23 @@
+#include "../minishell.h"
+
+void free_command_table(t_cmd *cmd)
+{
+
+}
+
+void free_tok_env(t_env *env_t)
+{
+
+}
+
+void free_env(char **ts)
+{
+
+}
+
+void free_tok(t_tok *t)
+{
+    free_command_table(t->command);
+    free_env_t(t->env_token);
+    free_env(t->env);
+}
