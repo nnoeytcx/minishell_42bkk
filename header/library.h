57338f392469b94bc1e959f0c0c42c5a368c9cc3
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-11-30 13:33:54 +0700

    add <string.h> libary for test

--- header/library.h
+++ header/library.h
@@ -10,7 +10,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 #include <errno.h> //<<< to add errno
-
+#include <string.h> // <<< add for strrerror << have to del later maybe
 #include "./define_var.h"
 #include "../libft/libft.h"
 #include "./struct.h"
