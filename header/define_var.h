728fd904c8462533a146ade4d35eb5b8d2785fa4
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-12-14 21:24:10 +0700

    add the ref and adjust define structure

--- header/define_var.h
+++ header/define_var.h
@@ -1,7 +1,7 @@
 #ifndef DEFINE_VAR_H
 #define DEFINE_VAR_H
 
-enum type {
+enum e_type {
     def = 0,
     string,
     variable,
@@ -18,24 +18,28 @@ enum type {
     redin_append_symbol
 };
 
-#define DEF_VAL 0
-#define ARG_STR 1
+// #define DEF_VAL 0
+// #define ARG_STR 1
 
-#define REDIR_IN 2
-#define REDIR_OUT 3
+// #define REDIR_IN 2
+// #define REDIR_OUT 3
 
-#define DOUBLE_Q 4
-#define SIN_Q_STR 5
+// #define DOUBLE_Q 4
+// #define SIN_Q_STR 5
 
-#define VAR_STR 6
+// #define VAR_STR 6
 
-#define FILE_IN 7
-#define FILE_OUT 8
-#define FILE_OUT_APPEND 11
-#define FILE_OUT_APPEND_SY 12
+// #define FILE_IN 7
+// #define FILE_OUT 8
+// #define FILE_OUT_APPEND 11
+// #define FILE_OUT_APPEND_SY 12
 
-#define HERE_DOC 9
-#define DOC_CUT 10
+// #define HERE_DOC 9
+// #define DOC_CUT 10
+
+#define DEFAULT_PATH_VALUE "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:."
+// should define according to bash when the env isn't have the PATH variable when run the bash PATH should set to this value 
+// see bash/config.h to see more --> https://github.com/bminor/bash/blob/8868edaf2250e09c4e9a1c75ffe3274f28f38581/config-top.h#L65
 
 #define SYNTAX_ERR "Minishell:  syntax error near unexpected token `"
 #define PROMPT "MINISHELL "
