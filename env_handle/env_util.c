d076982c4caec2f15b9a287790e352bc6cbfb9d8
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-12-17 02:18:22 +0700

    clean more

--- env_handle/env_util.c
+++ env_handle/env_util.c
@@ -1,26 +1,5 @@
 #include "../header/minishell.h"
 
-t_env	*new_env(char	*key, char	*value, int	mode)
-{
-	t_env	*new;
-
-	if (key == NULL)
-		return (NULL);
-	new = ft_calloc(sizeof(t_env), 1);
-	new->next = NULL;
-	if (mode == O_KEYV)
-	{
-		new->key = ft_strdup(key);
-		new->value = ft_strdup(value);
-	}
-	else if (mode == O_WSTR)
-	{
-		new->key = getenv_key(key);
-		new->value = getenv_value(key);
-	}
-	return (new);
-}
-
 void	print_env(t_tok token)
 {
 	t_env *tmp = token.env_token;
