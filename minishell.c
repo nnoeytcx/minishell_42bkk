632c594108402569ae1e7f5cb8356296c2c2ab85
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-12-26 11:36:40 +0000

    pix color and cmd prompt

--- minishell.c
+++ minishell.c
@@ -3,16 +3,60 @@
 
 int g_signal = 0 ;
 
+char *get_prompt(t_tok token)
+{
+	char *prompt;
+	char *pwd;
+	char *user;
+	char *lst_status;
+	char *tmp;
+	
+	user = get_value_from_key("USER", token.env_token);
+	pwd = get_value_from_key("PWD", token.env_token);
+	lst_status = ft_itoa(g_signal);
+
+	prompt = ft_strjoin("\033[1;31m", user);
+	
+	free(user);
+	tmp = prompt;
+
+	prompt = ft_strjoin(prompt, "\033[1;32m -> \033[1;34m");
+	
+	free(tmp);
+	tmp = prompt;
+	
+	prompt = ft_strjoin(prompt, pwd);
+	
+	free(tmp);
+	free(pwd);
+	tmp = prompt;
+	
+	prompt = ft_strjoin(prompt, " \033[1;33m$");
+	
+	free(tmp);
+	tmp = prompt;
+	
+	prompt = ft_strjoin(prompt, lst_status);
+	
+	free(lst_status);
+	free(tmp);
+	tmp = prompt;
+	
+	prompt = ft_strjoin(prompt, "\033[1;37m > \033[0;97m");
+	free(tmp);
+
+	return (prompt);
+}
 char *readline_input(t_tok token)
 {
 	char *input;
 	char *pwd;
+	char *prompt;
 
-	pwd = get_value_from_key("PWD", token.env_token);
-	ft_putstr_fd(pwd, 1);
-	ft_putstr_fd(" ", 1);
-	ft_putnbr_fd(g_signal, 1);
-	input = readline(" >"); //<< PROMPT in the header
+	prompt = get_prompt(token);
+	dprintf(2,"\nprompt = [%s]\n", prompt);
+	input = readline(prompt); //<< PROMPT in the header
+	dprintf(2,"\nintput = [%s]\n", input);
 	if (input == NULL) // << check case EOF or ^C kub 
 	{
 		rl_clear_history(); //<< safety 1st
@@ -20,7 +64,6 @@ char *readline_input(t_tok token)
 	}
 	if (!(0 == ft_strlen(input)))
 		add_history(input); // << กด ขึ้นเพื่อดู command ก่แนหน้าได้
-	free(pwd);
 	return (input);	
 }
 
@@ -36,11 +79,16 @@ int main(int ac , char **av, char **env)
 	while (1)
 	{
 		input = readline_input(token);
+		dprintf(2,"\nprompt == [%s]\n", input);
 		lexer_parser(&token, input);
+		dprintf(2,"\n--------out of parser--------\n");
+		print_tok(token);
+		dprintf(2,"\n-----------------------------\n");
 		if (0 == ft_strncmp("env", input, 3))
 			print_env(token);
-		print_tok(token);
+		dprintf(2,"\n--------------[exe]--------------\n");
 		g_signal = exe_command(&token);
+		dprintf(2,"\n---------------------------------\n");
 		// free_input_and_cmd_token(input, &token.command);
 		free(input);
 		input = NULL;
