dd646f56efdd57d5cdd9c52835d1f6b4294104e8
ESZU <57306907+EsperSaN@users.noreply.github.com>
2023-12-26 15:47:54 +0000

    rm the promt thing

--- minishell.c
+++ minishell.c
@@ -2,41 +2,43 @@
 
 char *get_prompt(t_tok token)
 {
-	char *temps[3];
-	
-	temps[0] = get_value_from_key("USER", token.env_token);
-	temps[2] = ft_strjoin("\033[1;31m", temps[0]);
-	free(temps[0]);
-	temps[0] = get_value_from_key("PWD", token.env_token);
-	temps[1] = temps[2];
-	temps[2] = ft_strjoin(temps[2], "\033[1;32m -> \033[1;34m");
-	free(temps[1]);
-	temps[1] = temps[2];
-	temps[2] = ft_strjoin(temps[2], temps[0]);
-	free(temps[1]);
-	free(temps[0]);
-	temps[0] = ft_itoa(token.return_code);
-	temps[1] = temps[2];
-	temps[2] = ft_strjoin(temps[2], " \033[1;33m$");
-	free(temps[1]);
-	temps[1] = temps[2];
-	temps[2] = ft_strjoin(temps[2], temps[0]);
-	free(temps[0]);
-	free(temps[1]);
-	temps[1] = temps[2];
-	temps[2] = ft_strjoin(temps[2], "\033[1;37m > \033[0;97m");
-	free(temps[1]);
-	return (temps[2]);
+	char *temps_0;
+	char *temps_1;
+	char *prompt;
+
+	temps_0 = get_value_from_key("USER", token.env_token);
+	prompt = ft_strjoin("\033[1;31m", temps_0);
+	free(temps_0);
+	temps_0 = get_value_from_key("PWD", token.env_token);
+	temps_1 = prompt;
+	prompt = ft_strjoin(prompt, "\033[1;32m -> \033[1;34m");
+	free(temps_1);
+	temps_1 = prompt;
+	prompt = ft_strjoin(prompt, temps_0);
+	free(temps_1);
+	free(temps_0);
+	temps_0 = ft_itoa(token.return_code);
+	temps_1 = prompt;
+	prompt = ft_strjoin(prompt, " \033[1;33m$");
+	free(temps_1);
+	temps_1 = prompt;
+	prompt = ft_strjoin(prompt,temps_0);
+	free(temps_0);
+	free(temps_1);
+	temps_1 = prompt;
+	prompt = ft_strjoin(prompt, "\033[1;37m > \033[0;97m");
+	free(temps_1);
+	return (prompt);
 }
 
 char *readline_input(t_tok token)
 {
 	char *input;
 	char *pwd;
-	char *prompt;
+	// char *prompt;
 
-	prompt = get_prompt(token);
-	input = readline(prompt); //<< PROMPT in the header
+	// prompt = get_prompt(token);
+	input = readline("minishell > "); //<< PROMPT in the header
 	if (input == NULL) // << check case EOF or ^C kub 
 	{
 		rl_clear_history(); //<< safety 1st
