7d6c94fd71b5c544558c7aac3f59a5b59722c6fd
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2023-12-30 15:19:53 +0700

    fix segfaukt in echo

--- builtin/echo.c
+++ builtin/echo.c
@@ -2,21 +2,27 @@
 
 int	ft_echo(char **param)
 {
-	int option;
+	int	option;
+	int	i;
 
-	option = 0;
 	if (param == NULL)
 		return (dprintf(2, "ECHO : param is NULL"), 1);
-
-	param++;
-	if (str_n_compare(*param, "-n", ft_strlen(*param)))
-		option == 1;
-	while (*param)
+	dprintf(2,"test\n");
+	i = 0;
+	while (param[i])
+		i++;
+	option = 0;
+	if (i < 2)
+		return (0);
+	if (str_n_compare(param[1], "-n", ft_strlen(param[1])))
+		option = 1;
+	i = 1;
+	while (param[i])
 	{
-		printf("%s ", *param);
-		if (option == 1)
+		printf("%s ", param[i]);
+		if (option != 1)
 			printf("\n");
-		param++;
+		i++;
 	}
 	return (0);
 }
