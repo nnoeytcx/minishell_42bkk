148f5ce455dc9944d8a9695155f0b7b696f94af1
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-01-01 16:52:59 +0700

    fix warn

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 01:52:56 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/01/01 16:42:47 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/01/01 16:52:23 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_exit(char **param, t_tok *token)
 {
-	(void *)param;
-	(void *)token;
+	(void)param;
+	(void)token;
 	dprintf(2, "EXIT RUNNNNNNN\n");
 	free_token(token);
 	exit(0);
--- builtin/unset.c
+++ builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 01:54:32 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/01/01 16:43:27 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/01/01 16:52:30 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_unset(char **param, t_tok *token)
 {
-	(void *)param;
-	(void *)token;
+	(void)param;
+	(void)token;
 	dprintf(2, "IN unset!!!!\n");
 	return (0);
 }
