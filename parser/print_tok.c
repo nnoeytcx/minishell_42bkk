d2fb9a277dd6862b2c9323a43074216f0ae5e13b
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-01-01 08:35:45 +0000

    change the out str to STERR

--- parser/print_tok.c
+++ parser/print_tok.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_tok.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 00:04:12 by tpoungla          #+#    #+#             */
-/*   Updated: 2023/12/30 18:03:14 by tpoungla         ###   ########.fr       */
+/*   Updated: 2024/01/01 05:00:39 by pruenrua         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,19 @@ void	print_command_tab(t_cmd *cmd)
 	while (tmp != NULL)
 	{
 		stt = tmp->str_mode;
-		printf("\n - {%d} cmd [%p] -\n\n", k, cmd);
+		dprintf(2,"\n - {%d} cmd [%p] -\n\n", k, cmd);
 		i = 0;
 		while (stt)
 		{
-			printf("[%s] mode [%d] -> ", stt->value, stt->type);
+			dprintf(2,"[%s] mode [%d] -> ", stt->value, stt->type);
 			stt = stt->next;
 			i++;
 		}
-		printf("\n\npid[%d]\nstatus[%d]", tmp->process_id, tmp->process_status);
+		//dprintf(2,"\n\npid[%d]\nstatus[%d]", tmp->process_id, tmp->process_status);
 		k++;
 		tmp = tmp->next;
 	}
-	printf ("\n\033[0;97m");
+	dprintf (2,"\n\033[0;97m");
 }
 
 void	print_tok(t_tok t)
