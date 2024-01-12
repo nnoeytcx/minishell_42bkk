4f069df5c07eeeb8e790708022fa1836ce9d81ef
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-01-12 13:14:37 +0700

    add free and split function

--- free/free.c
+++ free/free.c
@@ -1,1 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 13:06:06 by pruenrua          #+#    #+#             */
+/*   Updated: 2024/01/12 13:06:24 by pruenrua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../header/minishell.h"
+
+void	*ft_free(void *ptr)
+{
+	free(ptr);
+	return (NULL);
+}
+
+char	**free2d(char	**ptr)
+{
+	int	i;
+
+	i = 0;
+	if (!ptr)
+		return (NULL);
+	while (ptr[i])
+	{
+		ptr[i] = ft_free(ptr[i]);
+		i++;
+	}
+	ptr = ft_free(ptr);
+	return (NULL);
+}
